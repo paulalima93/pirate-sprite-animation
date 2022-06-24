b65b8495c606e183801f4343c136f88bb26cf75e
paulalima93 <91716314+paulalima93@users.noreply.github.com>
2022-06-24 17:15:47 -0300

    Create add aula 28.js

---
+++ add aula 28.js
@@ -0,0 +1,35 @@
+//criar var no sketch
+
+
+var boatAnimation = [];
+var boatSpritedata, boatSpritesheet;
+
+var waterSplashAnimation = [];
+var waterSplashSpritedata, waterSplashSpritesheet;
+
+
+//no preload
+
+  boatSpritedata = loadJSON("assets/boat/boat.json");
+  boatSpritesheet = loadImage("assets/boat/boat.png");
+  waterSplashSpritedata = loadJSON("assets/water_splash/water_splash.json");
+  waterSplashSpritesheet = loadImage("assets/water_splash/water_splash.png");
+
+
+//no setup
+
+var boatFrames = boatSpritedata.frames;
+  for (var i = 0; i < boatFrames.length; i++) {
+    var pos = boatFrames[i].position;
+    var img = boatSpritesheet.get(pos.x, pos.y, pos.w, pos.h);
+    boatAnimation.push(img);
+  }
+
+  var waterSplashFrames = waterSplashSpritedata.frames;
+  for (var i = 0; i < waterSplashFrames.length; i++) {
+    var pos = waterSplashFrames[i].position;
+    var img = waterSplashSpritesheet.get(pos.x, pos.y, pos.w, pos.h);
+    waterSplashAnimation.push(img);
+}
+
+
